a1a9da3811b2327ae4b185d4daacfcfe88973c7a
Cheney <qcuncle@gmail.com>
2024-03-11 21:47:19 +0800

    Update app version to 0.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "me.qcuncle.nowinnews"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
-        versionName = "0.0.2"
+        versionCode = 3
+        versionName = "0.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
